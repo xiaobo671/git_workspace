74b62c9861a9c1deb70da9670cb6cf3875c6b480
sunxiaobo <xiaobo671@126.com>
2020-12-31 11:20:25 +0800

    test:list contains方法测试

--- springbootdemo/src/test/java/com/ist/springbootdemo/SpringbootdemoApplicationTests.java
+++ springbootdemo/src/test/java/com/ist/springbootdemo/SpringbootdemoApplicationTests.java
@@ -3,12 +3,22 @@ package com.ist.springbootdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class SpringbootdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 		//test 
+	}
+
+	@Test
+	void listContains(){
+		List<String> list = new ArrayList<>();
+		list.add("qqq");
+		System.out.println(list.contains("qqq"));
 	}
 
 }
